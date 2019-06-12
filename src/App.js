3c92bb4e4fb611abeb90b175161e0fe5a6080d3b
Matthijs Hoek <matthijs.hoek@hotmail.nl>
2019-06-12 15:00:48 +0200

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/js/app';
+import './assets/styles/app.scss';
+import Sidebar from'./components/sidebar';
+
+function App() {
+  return (
+    <div className="container">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
